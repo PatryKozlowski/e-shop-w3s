6f50a96a5781bb7f2fc7110323027656a5d59714
PatryKozlowski <patrykozlowski0@gmail.com>
2022-12-07 20:44:53 +0100

    Eslint

--- components/MobileHeader.tsx
+++ components/MobileHeader.tsx
@@ -1,6 +1,6 @@
 import Logo from './Logo'
 
-const MobileHeader = () => {
+const MobileHeader = (): JSX.Element => {
   return (
     <header className={'flex lg:hidden px-4 bg-black text-white'}>
       < Logo />
