0763111c440321759891fa00e2abff1934039872
PatryKozlowski <patrykozlowski0@gmail.com>
2022-12-11 10:01:15 +0100

    Hidden input in mobile device

--- components/Header.tsx
+++ components/Header.tsx
@@ -15,12 +15,21 @@ const Header = (): JSX.Element => {
       return pageName[1].charAt(0).toUpperCase() + pageName[1].slice(1)
     }
   }, [])
+
+  const isNotHomeRoute = headerTitleFromRoute(route) === ''
+
+  console.log(isNotHomeRoute)
   return (
     <header className={'px-4 lg:px-0 w-full'}>
-      <div className={'flex items-center justify-between'}>
-        <p className={'my-6 text-2xl font-light'}>{headerTitleFromRoute(route)}</p>
+      <div className={`flex items-center p-2 ${isNotHomeRoute ? 'justify-end' : 'justify-between'} `}>
+        {
+          !isNotHomeRoute ?
+            <p className={'my-6 text-2xl font-light'}>{headerTitleFromRoute(route)}</p>
+            :
+            null
+        }
         <div className={'flex items-center space-x-4 my-4'}>
-          <div className={`flex items-center  ${isShowSearchInput ? 'border border-black' : 'border-none'} transition-all ease-in-out duration-700`}>
+          <div className={`hidden lg:flex items-center  ${isShowSearchInput ? 'border border-black' : 'border-none'} transition-all ease-in-out duration-700`}>
             <input
               placeholder={'Search...'}
               className={`p-2 border-none ${isShowSearchInput ? 'opacity-100 focus:outline-none' : 'opacity-0'} transition-all ease-in-out duration-700`}
